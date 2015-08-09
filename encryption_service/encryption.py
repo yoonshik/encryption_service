0272ec83b64b3034c0567d5b1a1edb4cbc4b9624
yoonshik <yoonshik@gmail.com>
2015-08-09 09:25:59 -0400

    first commit

---
+++ encryption_service/encryption.py
@@ -0,0 +1,34 @@
+from cryptography.fernet import Fernet
+
+#key = Fernet.generate_key()
+#cipher_suite = Fernet(key)
+#cipher_text = cipher_suite.encrypt(b"A really secret message. Not for prying eyes.")
+#plain_text = cipher_suite.decrypt(cipher_text)
+
+def generate_key():
+    key = Fernet.generate_key()
+    return key
+
+def get_cipher_suite(key):
+    return Fernet(key)
+
+def encrypt(plain_text, key):
+    cipher_suite = Fernet(key)
+    cipher_text = cipher_suite.encrypt(plain_text)
+    return cipher_text
+
+def decrypt(cipher_text, key):
+    cipher_suite = Fernet(key)
+    plain_text = cipher_suite.decrypt(cipher_text)
+    return plain_text
+
+key = generate_key()
+print "KEY: " + key
+
+message = b"A really secret message. Not for prying eyes."
+
+cipher_text = encrypt(message, key)
+print "CIPHER_TEXT: " + cipher_text
+
+plain_text = decrypt(cipher_text, key)
+print "PLAIN_TEXT: " + str(plain_text)
